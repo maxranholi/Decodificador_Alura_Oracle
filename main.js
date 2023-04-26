8a7973b43624c7bc4f6e887a68c255db90763478
Max-Ranholi <ranholimax@gmail.com>
2023-04-26 19:34:47 -0300

    teste de caso para campo vazio feito

--- main.js
+++ main.js
@@ -2,50 +2,89 @@ let input = document.querySelector("textarea");
 let showSection = document.querySelector(".image");
 let message = document.querySelector(".message");
 let messageOut = document.querySelector(".subTitle");
+let messageResults = document.querySelector(".messageResult");
+
+function checkEmptyValue() {
+  if (input.value === "") {
+    alert("Digite algum texto");
+  }
+}
 
 function showEncriptedMessage() {
-  let str = input.value;
-  const mapObj = {
-    a: "ai",
-    e: "enter",
-    i: "imes",
-    o: "ober",
-    u: "ufat",
-  };
-  let newString = str.replace(/a|e|i|o|u/gi, function (matched) {
-    return mapObj[matched];
-  });
-  return newString;
+  checkEmptyValue();
+  if (/^[A-Z0-9]+$/i.test(input.value)) {
+    if (!/[!@#$%^&*(),.?":{}|<>]/.test(input.value)) {
+      return alert(
+        "Por favor, apenas letras minúsculas e sem caracteres especiais."
+      );
+    } else {
+      return false;
+    }
+  } else {
+    let str = input.value;
+    const mapObj = {
+      a: "ai",
+      e: "enter",
+      i: "imes",
+      o: "ober",
+      u: "ufat",
+    };
+    let newString = str.replace(/a|e|i|o|u/gi, function (matched) {
+      return mapObj[matched];
+    });
+    return newString;
+  }
 }
 
 function showDecriptedMessage() {
-  let str = input.value;
-  const mapObj = {
-    ai: "a",
-    enter: "e",
-    imes: "i",
-    ober: "o",
-    ufat: "u",
-  };
-  let newString = str.replace(/ai|enter|imes|ober|ufat/gi, function (matched) {
-    return mapObj[matched];
-  });
-  return newString;
+  checkEmptyValue();
+  if (/^[A-Z0-9]+$/i.test(input.value)) {
+    if (!/[!@#$%^&*(),.?":{}|<>]/.test(input.value)) {
+      return alert(
+        "Por favor, apenas letras minúsculas e sem caracteres especiais."
+      );
+    } else {
+      return false;
+    }
+  } else {
+    let str = input.value;
+    const mapObj = {
+      ai: "a",
+      enter: "e",
+      imes: "i",
+      ober: "o",
+      ufat: "u",
+    };
+    let newString = str.replace(
+      /ai|enter|imes|ober|ufat/gi,
+      function (matched) {
+        return mapObj[matched];
+      }
+    );
+    return newString;
+  }
 }
 
 function encripMessage() {
-  message.textContent = showEncriptedMessage();
+  messageResults.textContent = showEncriptedMessage();
 }
-document.querySelector(".buttonCrip").addEventListener("click", encripMessage);
-
-function decripMessage() {
-  message.textContent = showDecriptedMessage()
-}
-
-document.querySelector(".buttonDescrip").addEventListener("click", decripMessage)
-
 
 document.querySelector(".buttonCrip").addEventListener("click", function () {
+  encripMessage();
   showSection.style.display = "none";
   messageOut.style.display = "none";
+  message.style.display = "none";
+  messageResults.style.display = "flex";
+});
+
+function decripMessage() {
+  messageResults.textContent = showDecriptedMessage();
+}
+
+document.querySelector(".buttonDescrip").addEventListener("click", function () {
+  decripMessage();
+  showSection.style.display = "none";
+  messageOut.style.display = "none";
+  message.style.display = "none";
+  messageResults.style.display = "flex";
 });
